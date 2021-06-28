e1f6fd3e74e66e85ee9eb030243f31e964c2eca7
davidlangdale <david.langdale@global.ntt>
2021-06-28 11:42:32 +1000

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -28,7 +28,7 @@ class Site:
     def run_parser(self, path):
         parser = self.load_parser(self, path.suffix)
         if parser is not None:
-            parser.parse(path, self.source, self.dest)
+            return parser.parse(path, self.source, self.dest)
         else:
             print('Not Implemented')
 
