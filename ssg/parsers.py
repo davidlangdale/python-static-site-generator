7916509c1129ff3d0da67ba8625382149e9fd8a8
davidlangdale <david.langdale@global.ntt>
2021-06-28 12:34:51 +1000

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -4,34 +4,33 @@ from typing import List
 from pathlib import Path
 
 
-def __init__():
+class Parser:
+    extensions: List[:str] = []
 
-    class Parser:
-        extensions: List[:str] = []
+    def valid_extension(self, extension):
+        return extension in self.extensions
 
-        def valid_extension(self, extension):
-            return extension in self.extensions
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
 
-        def parse(self, path: Path, source: Path, dest: Path):
-            raise NotImplementedError
+    def read(self, path: Path):
+        with path.open() as file:
+            contents = file.read()
+            return contents
 
-        def read(self, path: Path):
-            with path.open() as file:
-                contents = file.read()
-                return contents
+    def write(self, path: Path, dest: Path, content, ext='.html'):
+        destination = dest
+        full_path = destination / path.with_suffix(ext).name
+        with full_path.open() as file:
+            full_path.write_bytes(content)
 
-        def write(self, path: Path, dest: Path, content, ext='.html'):
-            destination = dest
-            full_path = destination / path.with_suffix(ext).name
-            with full_path.open() as file:
-                full_path.write_bytes(content)
+    def copy(self, path, source, dest):
+        shutil.copy2(path, path.relative_to(source) / dest)
 
-        def copy(self, path, source, dest):
-            shutil.copy2(path, path.relative_to(source) / dest)
-
-        class ResourceParser(extensions=['.jpg', '.png', '.gif', '.css', '.html']):
-            def parse(self, path: Path, source: Path, dest: Path):
-                self.copy(path, source, dest)
+class ResourceParser(Parser):
+    extensions = ['.jpg', '.png', '.gif', '.css', '.html']
+    def parse(self, path: Path, source: Path, dest: Path):
+        self.copy(path, source, dest)
 
 
 
--- ssg/site.py
+++ ssg/site.py
@@ -12,6 +12,18 @@ class Site:
         print(f'directory {directory}')
         directory.mkdir(parents=True, exist_ok=True)
 
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
+
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print('Not Implemented')
+
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
@@ -19,19 +31,6 @@ class Site:
                 self.create_dir(path)
             elif path.is_file():
                 self.run_parser(path)
-
-    def load_parser(self, extension):
-        for parser in self.parsers:
-            if parser.valid_extension(extension):
-                return parser
-
-    def run_parser(self, path):
-        parser = self.load_parser(self, path.suffix)
-        if parser is not None:
-            parser.parse(path, self.source, self.dest)
-        else:
-            print('Not Implemented')
-
 
 
 
