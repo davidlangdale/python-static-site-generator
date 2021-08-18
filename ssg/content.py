285f746076beeb7eeb47b1a5233eebaff6a4ce71
David Langdale <David.Langdale@global.ntt>
2021-08-19 08:46:05 +1000

    Test

--- ssg/content.py
+++ ssg/content.py
@@ -1,19 +1,20 @@
 import re
+from abc import ABC
+from copy import deepcopy
 
 from yaml import load, FullLoader
 from collections.abc import Mapping
 
 
-
-class Content(Mapping):
+class Content(Mapping, ABC):
     __delimiter = {r"^(?:-|\+){3}\s*$"}
     __regex = re.compile(__delimiter, re.MULTILINE)
 
     @classmethod
     def load(cls, string):
         _ = Content.__regex.split(string, 2)
-        fm = _
-        content = _
+        fm = deepcopy(_)
+        content = deepcopy(_)
         load(fm, Loader=FullLoader)
         return cls(metadata, content)
 
