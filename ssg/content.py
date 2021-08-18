57d5d559a4de7d79c1c052565e6d188f65a6bab2
David Langdale <David.Langdale@global.ntt>
2021-08-18 13:24:52 +1000

    Test

---
+++ ssg/content.py
@@ -0,0 +1,19 @@
+import re
+from yaml import load
+from yaml import FullLoader
+from collections.abc import Mapping
+
+class Content():
+    __delimiter = (r"^(?:-|\+){3}\s*$")
+    __regex = re.compile(__delimiter, re.MULTILINE)
+
+    @classmethod
+    def load(cls, string):
+        _ = Content.__regex.split(string, 2)
+        fm = _
+        content = _
+
+    load(fm, Loader=FullLoader)
+    cls(metadata, content)
+
+
