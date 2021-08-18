0cb22e2a0c252f7c446adc7127770f5d13f17711
David Langdale <David.Langdale@global.ntt>
2021-08-19 09:05:18 +1000

    Test

--- ssg/content.py
+++ ssg/content.py
@@ -5,7 +5,7 @@ from collections.abc import Mapping
 
 
 class Content(Mapping, ABC):
-    __delimiter = {r"^(?:-|\+){3}\s*$"}
+    __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
 
     @classmethod
