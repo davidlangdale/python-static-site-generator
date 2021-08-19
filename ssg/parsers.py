6c42275a526093cdc820549bb50ce88799461b89
David Langdale <David.Langdale@global.ntt>
2021-08-19 12:20:38 +1000

    Test

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -29,6 +29,9 @@ class Parser:
     def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
 
+    class MarkdownParser:
+        extensions = [".md", ".markdown"]
+
 class ResourceParser(Parser):
     extensions = ['.jpg', '.png', '.gif', '.css', '.html']
     def parse(self, path: Path, source: Path, dest: Path):
@@ -36,8 +39,7 @@ class ResourceParser(Parser):
 
 
 
-class MarkdownParser:
-    extensions = [".md", ".markdown"]
+
 
 
 
