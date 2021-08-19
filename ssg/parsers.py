2361c12e188ad14944523ffb5e02e7d1f3868c53
David Langdale <David.Langdale@global.ntt>
2021-08-19 15:45:49 +1000

    Test

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -46,7 +46,8 @@ class MarkdownParser(Parser):
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
 
-
+class ReStructuredTextParser:
+    extensions = [".rst"]
 
 
 
